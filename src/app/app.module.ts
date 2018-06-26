0f265325c85116b08a1a1bc573c7ab3086bd6b5e
Ralph <rforgeon@gmail.com>
2018-06-26 16:23:42 +0100

    Remove firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,14 +18,6 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 import { firebaseConfig } from './firebaseConfig';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAVBcynX3OpMFuJcLfjmpYYQSiQKdIhOhg",
-//     authDomain: "lifi-b9d3b.firebaseapp.com",
-//     databaseURL: "https://lifi-b9d3b.firebaseio.com",
-//     projectId: "lifi-b9d3b",
-//     storageBucket: "lifi-b9d3b.appspot.com",
-//     messagingSenderId: "547729101775"
-//   };
 
 @NgModule({
   declarations: [
