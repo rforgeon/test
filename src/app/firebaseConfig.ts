07ac19361abf8111093ee4a153dc308d0f25eddf
Ralph <rforgeon@gmail.com>
2018-06-26 08:13:12 +0100

    remove firebase credentials

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAVBcynX3OpMFuJcLfjmpYYQSiQKdIhOhg",
-  authDomain: "lifi-b9d3b.firebaseapp.com",
-  databaseURL: "https://lifi-b9d3b.firebaseio.com",
-  projectId: "lifi-b9d3b",
-  storageBucket: "lifi-b9d3b.appspot.com",
-  messagingSenderId: "547729101775"
+  apiKey: "xxxxxxxxxx",
+  authDomain: "example.firebaseapp.com",
+  databaseURL: "example.firebaseio.com",
+  projectId: "example",
+  storageBucket: "example",
+  messagingSenderId: "12345"
 }
